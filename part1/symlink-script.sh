35420a6f6284cb6afd7e09c947843a4e893b34c9
jades <jadestarr99@gmail.com>
2024-11-09 03:58:02 +0000

    symbolic links created and working, not commented

--- part1/symlink-script.sh
+++ part1/symlink-script.sh
@@ -4,5 +4,9 @@ if [ $(id -u) -ne 0 ]
     then echo "run this script as root <sudo ./symlink-script>"
     exit
 else
-    ln -sf .config/kak/kakrc ~/bin
+    mkdir -p "~/.config/kak"
+    mkdir -p "~/.config/tmux"
+
+    ln -sf ~/Asg2Start/config/tmux/tmux.conf ../.config/tmux
+    ln -sf ~/Asg2Start/config/kak/kakrc ../.config/kak
 fi
