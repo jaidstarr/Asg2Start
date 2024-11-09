eb1fda8f809def7a062a134398e215b0a84db74c
jades <jadestarr99@gmail.com>
2024-11-09 05:13:21 +0000

    main.sh updated to have sudo error handeling and comments

--- part1/main.sh
+++ part1/main.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-./install-script.sh #runs the script to install the packages
+if [ $(id -u) -ne 0 ]  # if this script is run by itself, this line will catch the users ID (UID) with "id -u" and see if it is not equal (-ne) to 0. if a UID is 0 then they have root privileges and would not need to use sudo.
+  then echo "please run this script as root <sudo ./main.sh>"
+  exit
 
-./symlink-script.sh #runs the script to create the symbolic links
+else
+  ./install-script.sh  # runs the script to install the packages
+
+  ./symlink-script.sh  # runs the script to create the symbolic links 
+fi
 
