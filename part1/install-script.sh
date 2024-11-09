5e339339222d8b83365740443d012bbab85b2987
jades <jadestarr99@gmail.com>
2024-11-09 05:01:14 +0000

    comments added to install-script and symlink-script. references added to all previous comments

--- part1/install-script.sh
+++ part1/install-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ $(id -u) -ne 0 ]  # if this script is run by itself, this line will check the users ID (UID) with "id -u" and see if it is not equal (-ne) to 0. if a UID is 0 then they have root privileges and would not need to use sudo.
+if [ $(id -u) -ne 0 ]  # if this script is run by itself, this line will check the users ID (UID) with "id -u" and see if it is not equal (-ne) to 0. if a UID is 0 then they have root privileges and would not need to use sudo. <https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script>
     then echo "run this script as root <sudo ./install-script>"
     exit
 else
-  cat packages.txt | while read line  # cat is used to read the text files contents. |(pipe) takees the output of the file and turns it into an input for "while read line" each line of te file gets stored in the variable $line. 
+  cat packages.txt | while read line  # cat is used to read the text files contents. |(pipe) takees the output of the file and turns it into an input for "while read line" each line of te file gets stored in the variable $line. <https://www.javatpoint.com/bash-read-file> <https://stackoverflow.com/questions/1521462/looping-through-the-content-of-a-file-in-bash>
     do 
-        pacman -S --noconfirm $line #pacman is the page installer that will then install each pakage into the system. "--noconfirm" makes it so you will not need to hit y to install each pakage
+        pacman -S --noconfirm $line #pacman is the page installer that will then install each pakage into the system. "--noconfirm" makes it so you will not need to hit y to install each pakage. <https://bbs.archlinux.org/viewtopic.php?id=121792>
     done
 fi
--- part1/symlink-script.sh
+++ part1/symlink-script.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [ $(id -u) -ne 0 ]
+if [ $(id -u) -ne 0 ]  # if this script is run by itself, this line will catch the users ID (UID) with "id -u" and see if it is not equal (-ne) to 0. if a UID is 0 then they have root privileges and would not need to use sudo <https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script>
     then echo "run this script as root <sudo ./symlink-script>"
     exit
 else
+  # mkdir creates a new directory, -p makes sure there are the correct parent directories. in this case, the parent dir ".config" is getting created as well as the "kak" dir being created inside the parent. <https://stackoverflow.com/questions/20089586/what-does-the-mkdir-p-mean-in-a-script-file#:~:text=mkdir%20-p%20means%3A%20create%20the,if%20required%2C%20all%20parent%20directories.>
     mkdir -p "~/.config/kak"
     mkdir -p "~/.config/tmux"
 
+    # ln creates a new symbolic link. <https://www.freecodecamp.org/news/linux-ln-how-to-create-a-symbolic-link-in-linux-example-bash-command/> -sf is used to update the link rather then using just "-s" which would not ovewrite if the script was already run. <https://stackoverflow.com/questions/1951742/how-can-i-symlink-a-file-in-linux> the first path is the path to the actual file and the second path is where you want the destination path to be.
     ln -sf ~/Asg2Start/config/tmux/tmux.conf ../.config/tmux
     ln -sf ~/Asg2Start/config/kak/kakrc ../.config/kak
 fi
